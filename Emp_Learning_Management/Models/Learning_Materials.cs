709c139d60a561c839810fa6a4204a49238fc402
vardhan chikkudu <vardhan.chikkudu@winwire.com>
2026-03-18 23:20:45 +0530

    Add project files.

---
+++ Emp_Learning_Management/Models/Learning_Materials.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Emp_Learning_Management.Models
+{
+    public class Learning_Materials
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+        
+
+
+    }
+}
