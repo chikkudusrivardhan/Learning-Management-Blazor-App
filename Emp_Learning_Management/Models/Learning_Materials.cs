83053f853fb7ea000da444a5eca6628e039a4120
vardhan chikkudu <vardhan.chikkudu@winwire.com>
2026-03-18 23:23:06 +0530

    feature string

--- Emp_Learning_Management/Models/Learning_Materials.cs
+++ Emp_Learning_Management/Models/Learning_Materials.cs
@@ -10,6 +10,8 @@ namespace Emp_Learning_Management.Models
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
+
+        public string new_name { get; set; } = "feature";
         
 
 
